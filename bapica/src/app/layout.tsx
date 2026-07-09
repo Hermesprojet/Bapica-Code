dd58bd5bcd99bcc8c21f50328571cc2a9e3bea8a
Bapica <abdelhakimelmokrefi@gmail.com>
2026-07-09 21:56:14 +0000

    fix: theme-color meta to #0d0d1a

--- bapica/src/app/layout.tsx
+++ bapica/src/app/layout.tsx
@@ -116,7 +116,7 @@ export default function RootLayout({
       <head>
         {/* PWA */}
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#061126" />
+        <meta name="theme-color" content="#0d0d1a" />
         {/* JSON-LD Structured Data */}
         <JsonLdScript data={getOrganizationSchema() as unknown as Record<string, unknown>} />
         <JsonLdScript data={getWebSiteSchema() as unknown as Record<string, unknown>} />
